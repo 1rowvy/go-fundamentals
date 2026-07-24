e1e65c30c45a36802303cfbd6ef242cd8f67b7ec
1rowvy <viktor.aleshin2004@gmail.com>
2026-07-24 20:54:06 +0300

    done

--- main.go
+++ main.go
@@ -2,21 +2,16 @@ package main
 
 import "fmt"
 
-type Point struct {
-	X int
-	Y int
+func main() {
+	var n int
+
+	fmt.Scan(&n)
+
+	increment(&n)
+
+	fmt.Print(n)
 }
 
-func main() {
-	var x1, y1, x2, y2 int
-
-	fmt.Scan(&x1, &y1, &x2, &y2)
-
-	p1 := Point{x1, y1}
-	p2 := Point{x2, y2}
-
-	dx := p2.X - p1.X
-	dy := p2.Y - p1.Y
-
-	fmt.Print(dx*dx + dy*dy)
+func increment(n *int) {
+	*n++
 }
