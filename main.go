0d739dee9d05dbeb8d9c2c68dce9de06bcea7a78
1rowvy <viktor.aleshin2004@gmail.com>
2026-07-24 20:58:58 +0300

    done

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	var n int
+	var a, b int
 
-	fmt.Scan(&n)
+	fmt.Scan(&a, &b)
 
-	increment(&n)
+	res, err := safeDivide(a, b)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
 
-	fmt.Print(n)
+	fmt.Printf("result: %d", res)
 }
 
-func increment(n *int) {
-	*n++
+func safeDivide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("divide by zero")
+	}
+
+	return a / b, nil
 }
